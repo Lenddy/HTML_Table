6c4ff92263d48a141417f8f2c789df5295fa8178
Lenddy Morales <leme2003@gmail.com>
2022-06-13 03:36:01 -0400

    Html Table

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+users = [
+    {'first_name': 'Michael', 'last_name': 'Choi'},
+    {'first_name': 'John', 'last_name': 'Supsupin'},
+    {'first_name': 'Mark', 'last_name': 'Guillen'},
+    {'first_name': 'KB', 'last_name': 'Tonel'}
+]
+
+print(users ['first_name'])
